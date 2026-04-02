c17593e9f7929126d790697cbb681c2c782d1e3b
Shayan Nikbakht <shayan@mac.home>
2026-04-02 06:20:58 +0100

    add dependencies

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const app = express();
 
 // ====== CONFIG ======
-const CLIENT_ID = process.env."1488617439142084608";
+const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env."sHa5rwsBcgTVgzZgyjHHyPcl2bo2lNTC";
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const SHEET_URL = process.env."https://script.google.com/macros/s/AKfycbxEjWH4eRezJVw6Tdm9_2ojCgCQS_p0wDfztktR7WhsCaoFZoCCbp5i3_7o44iZglhl/exec";
